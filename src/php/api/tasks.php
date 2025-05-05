8beade728ed6c35e76ed8d5e0004d3bc2cfa5de5
Michael Mroz <michaelmroz01111@gmail.com>
2025-05-05 16:44:53 -0300

    added and tested POST route

--- src/php/api/tasks.php
+++ src/php/api/tasks.php
@@ -32,22 +32,71 @@ switch ($method) {
         break;
 }
 
+/**
+ * helper function to set headers and echo a response to HTTP requests
+ * @param $data the response body to send to the client
+ * @param $statusCode the status code of the response
+ */
+function respond(array $data, int $statusCode = 200) {
+    header('Content-Type: application/json');
+    http_response_code($statusCode);
+    echo json_encode($data);
+    exit;
+}
+
 function handleGet(PDO $pdo) {
     try {
         $stmt = $pdo->prepare(getTasksQuery());
         $stmt->execute();
         $res = $stmt->fetchAll(PDO::FETCH_ASSOC);
-        header('Content-Type: application/json');
-        echo json_encode($res);
-        return;
+        if (!$res) {
+            respond(['error' => 'Tasks not found'], 404);
+        }
+
+        respond($res);
     } catch (PDOException $e) {
-        header('Content-Type: application/json');
-        echo json_encode(['message' => 'GET request failed', 'error' => $e->getMessage()]);
+        respond(['error' => $e->getMessage()], 500);
     }
 }
 
+/**
+ * helper function to ensure task object exists and has valid content length <= 50 chars
+ * @param $taskArr an associative array of the task object
+ * @return true if $taskArr['content'] exists and is of length between [0->50] else false
+ */
+function validateContent($taskArr): bool {
+    if (!$taskArr || !$taskArr['content']) {
+        return false;
+    }
+
+    $length = strlen($taskArr['content']);
+    return $length > 0 && $length <= 50;
+}
+
+/**
+ * sanitizes the text input from the user
+ * @param $content the task's content, inputted by user
+ * @return string containing the sanitized input
+ */
+function sanitizeContent($content): string {
+    return htmlspecialchars($content, ENT_QUOTES, "UTF-8");
+}
+ 
 function handlePost(PDO $pdo, array $req) {
-    // to do: create function to validate the input length
+
+    if (!validateContent($req))  {
+        respond(['error' => 'Missing or malformatted task'], 400);
+    }
+
+    try {
+        $stmt = $pdo->prepare(postTaskQuery());
+        $req['content'] = sanitizeContent($req['content']);
+        $stmt->execute($req);
+        $res = ['id' => $pdo->lastInsertId(), 'content' => $req['content']];
+        respond($res, 201);
+    } catch (PDOException $e) {
+        respond(['error' => $e->getMessage()], 500);
+    }
 }
 
 function handlePut(PDO $pdo, array $req) {
--- src/php/db/queries.php
+++ src/php/db/queries.php
@@ -3,3 +3,8 @@
 function getTasksQuery():string {
     return "select * from tasks";
 }
+
+function postTaskQuery():string {
+    //note to self:  the : before content is needed for working w. assoc arr. 
+    return "insert into tasks (content) VALUES (:content)";
+}
